71d72c0d576374842e3c9edcbb5e10b9792c3bc5
mholdrege <mholdrege@gcp.usgs.gov>
2022-09-27 19:49:44 +0000

    seperating out part of the analysis to create an intermediate simple bin image

---
+++ scripts/02_compile_fire_data.js
@@ -0,0 +1,181 @@
+/*
+
+Purpose: Compile combined wildland fire data set over 35 years
+so that each pixel has a binary code that denotes which year(s)
+that pixel burned. Then replace that bin code with a 'simple'
+shorter code (fewer digits) so don't run out of numeric precision, ]
+in downstream scripts.  also create a key between the bin id and the
+new simple id. 
+
+Author: Martin Holdrege
+
+Started: 9/27/2022
+
+*/
+
+
+// Constants
+
+var pathAsset = 'projects/gee-guest/assets/newRR_metrics/';
+var scale = 30;
+var startYear = 1986;
+var endYear = 2020;
+
+
+/***************************
+
+load the data 
+
+****************************
+*/
+
+// suid--data layer from Daniel Schlaepfer. This is an 'id' layer, where each pixel
+// has a number which corresponds to the nrcs soil unit polygon that it belongs to. 
+// resolution is 30 m. Certain 'non drylands' have been masked out for this analysis. 
+// here just using this layer for masking
+
+var suid1 = ee.Image(pathAsset + 'suid/gsu_masked_v20220314')
+  .rename('suid')
+  .toInt64(); // 64bit b/ later create very long numeric codes. 
+
+Map.addLayer(suid1, {min: 0, max: 100000}, 'suid', false);
+
+var mask = suid1.unmask().neq(0).rename('mask');
+Map.addLayer(mask, {min: 0, max: 1, palette: ['white', 'black']}, 'mask', false);
+// region of interest
+
+var biome = ee.FeatureCollection("projects/gee-guest/assets/SEI/US_Sagebrush_Biome_2019"); // provided by DT
+
+var region = biome.geometry();
+
+// fire polygons
+// combined wildland fire dataset (from USGS--combines 40 different data sources)
+
+var cwf1 = ee.FeatureCollection(pathAsset + 'usgs_combined_wildland_fire_complete')
+  .filterBounds(region);
+  
+Map.addLayer(cwf1, {}, 'cwf', false);
+
+
+/*
+
+Prepare fire data for summarizing
+
+*/
+
+var zero = ee.Image(0);
+// create list of years
+var years = ee.List.sequence(startYear, endYear);
+var yearsCount = ee.List.sequence(1, ee.Number(years.length()));
+
+// vector 2^0, 2^1...to 2^34 (ie the values of each 'place' on a binary scale)
+var yearsBin = yearsCount.map(function(x) {
+  var exponent = ee.Number(x).subtract(1);
+  var out = ee.Number(2).pow(exponent);
+  return out; 
+});
+
+
+var cwfByYear = years.map(function(year) {
+  return cwf1.filter(ee.Filter.eq('Fire_Yr', year));
+});
+
+Map.addLayer(ee.FeatureCollection(cwfByYear.get(0)), {}, '1986');
+
+// one image for each year 0 if unburned, 1 if burned
+var cwfImageByYear = cwfByYear
+  .map(function(fc) {
+    return zero.paint(fc, 1).rename('fire');
+  })
+  .zip(years) // combine two lists into one (each element of list is a list w/ 2 elements)
+  .map(fnsC.setTimeStart) //
+  .map(function(image) {
+    // unburned areas are masked
+    return ee.Image(image).selfMask();
+  });
+
+//print('test', ee.FeatureCollection(cwfByYear.get(1)).first());
+Map.addLayer(ee.Image(cwfImageByYear.get(0)), {min:0, max: 1, palette: ['white', 'black']}, 'fires, yr 1', false);
+
+// in the year 1 image areas that burned are 1 (2^0)
+// in year 2 they are 2 (2^1), in year 3 they are 4 (2^2), ...,
+// etc until 2^34 (assuming 35 yrs total)
+var cwfBinImageByYear = cwfImageByYear
+  .zip(yearsBin)
+  .map(function(x) {
+    var image = ee.Image(ee.List(x).get(0));
+    var multiplier = ee.Number(ee.List(x).get(1));
+    var out = image.multiply(multiplier).cast({'fire':'int64'});
+    return out;
+  });
+  
+
+//Map.addLayer(ee.Image(cwfBinImageByYear.get(0)), {palette: 'black'}, 'bin 1986') 
+// summing across years the pixels that burned.
+// this creates a code, where converting the code from
+// integer  to binary (base 2) will tell you what year(s) burned.
+// for example if the value of a pixel is 9 that would mean that year 1 and year 4 burned
+// because 9 written in binary is 0000001001, where 1's denote years that burned and 0's denote
+// years that didn't burn 
+var cwfBinImage = ee.ImageCollection(cwfBinImageByYear).sum();
+
+// self mask here because summing acrros layers leads to 0s where
+// all the layers were masked
+
+var maskFire = cwfBinImage.unmask().neq(0).rename('mask'); //1 for burned areas
+var cwfBinImageM = cwfBinImage
+  .mask(maskFire)
+  .updateMask(mask)
+  .rename('bin');
+  
+Map.addLayer(cwfBinImageM, {min:0, max: 10^12, palette: ['Black']}, 'fires all yrs', false);
+
+
+// get all the unique 'binary' fire-year codes
+//(https://gis.stackexchange.com/questions/403785/finding-all-unique-values-in-categorical-image)
+var reduction = cwfBinImageM.reduceRegion({
+  reducer: ee.Reducer.frequencyHistogram(), 
+  geometry: region,
+  scale: scale,
+  maxPixels: 1e11
+});
+
+var binUnique = ee.Dictionary(reduction.get(cwfBinImageM.bandNames().get(0)))
+    .keys()
+    .map(ee.Number.parse)
+    .sort();
+    
+if(testRun) {
+  print('unique bin vals', binUnique);
+}
+
+print('length', binUnique.length());
+print(binUnique);
+
+// creating a new 'binSimple' which is a smaller number based on the
+// actual number of unique bins
+var binSimple = ee.List.sequence(ee.Number(1), binUnique.length())
+
+var binSimpleImageM = cwfBinImageM.int64().remap(binUnique, binSimple);
+
+
+// create key of bin (ie the actual binary code) and binSimple
+var binKey = binUnique.zip(binSimple)
+  .map(function(x) {
+    var f = ee.Feature(null, 
+      // using this code here to rename the parts as needed
+        {bin: ee.List(x).get(0),
+        binSimple: ee.List(x).get(1)
+      });
+    return f;
+  })
+
+var binKeyFc = ee.FeatureCollection(binKey);
+
+
+
+
+
+
+
+
