8f0252babf0553c848d5d27f87f6150ff8c7e060
Martin Holdrege <martinholdrege@gmail.com>
2022-11-01 14:27:13 -0600

    created clean csv of area and afg cover by suidBinSimple

--- scripts/04_download_GEE_output_from_gdrive.R
+++ scripts/04_download_GEE_output_from_gdrive.R
@@ -11,12 +11,13 @@
 library(googledrive)
 library(stringr)
 library(tidyverse)
+# for drive_download_from_df function
+source("../SEI/src/general_functions.R")
 
 # get file paths drive --------------------------------------------------------
 
 
-files1 <- drive_ls(path = "newRR_metrics",
-                        pattern = "(^area)|(^RAP_)")
+files1 <- drive_ls(path = "newRR_metrics")
 files1
 
 # download  ---------------------------------------------------------------
@@ -29,15 +30,49 @@ files2 <- files1 %>%
   filter(modifiedTime == max(modifiedTime))
 
 
+# select most recent date -------------------------------------------------
+# sometimes multiple files are loaded to gdrive over time,
+# and the date is appended to the file names, I only want the
+# one with the most recent date
+
+files2 <- files1 %>% 
+  mutate(name_no_date = str_replace(name, "20\\d{6}\\.", ""),
+         date = str_extract(name, "20\\d{6}\\."),
+         date = lubridate::ymd(date),
+         modifiedTime = map_chr(drive_resource, function(x) x$modifiedTime)) %>% 
+  # if multiple files with the same
+  # name only download the newer one
+  group_by(name) %>% 
+  filter(modifiedTime == max(modifiedTime),
+         # don't download files from test runs
+         !str_detect(name, "testRun")) %>% 
+  # if multiple files create with different date strings
+  # only keep the recent one 
+  group_by(name_no_date) %>% 
+  filter(date == max(date))
+files2
+
 # area files --------------------------------------------------------------
 
-
 area_files <- files2 %>%
-  filter(str_detect(name, '^area')) 
+  filter(str_detect(name, '^area.*\\.csv$')) 
 
-for (i in 1:nrow(area_files)) {
-  drive_download(file = area_files$id[i], 
-                 path = file.path("data_processed/", area_files$name[i]),
-                 overwrite = TRUE)
-}
+drive_download_from_df(area_files, folder_path = "data_processed/area")
+
+
+# RAP summary files -------------------------------------------------------
+
+rap_files <- files2 %>%
+  filter(str_detect(name, '^RAP.*\\.csv$')) 
+
+drive_download_from_df(rap_files, folder_path = "data_processed/RAP")
+
+
+# keys --------------------------------------------------------------------
+# key matching the bin id and binSimple id
+
+files2 %>%
+  filter(str_detect(name, '^key.*\\.csv$')) %>% 
+  drive_download_from_df(., folder_path = "data_processed/key")
+
                  
--- scripts/05_compile_GEE_output.R
+++ scripts/05_compile_GEE_output.R
@@ -15,40 +15,119 @@
 # dependencies ------------------------------------------------------------
 
 library(tidyverse)
-source('scripts/functions.R')
+source('src/functions.R')
+
+
+# constants ---------------------------------------------------------------
+
+date_string <- "20221101" # for appending to files
 
 # load data ---------------------------------------------------------------
 
 
 # * area ------------------------------------------------------------------
+# amount of area belong to each suidBinSimple
 
-area1 <- read_csv("data_processed/area-by-suidBin_20220925.csv",
+# keeping main portion of file name for naming of output files
+area_name_base <- "area-by-suidBinSimple_1986_2020_30m_"
+area1 <- read_csv(paste0("data_processed/area/", area_name_base, 
+                         "20221031.csv"),
                   col_types = 'cccc')
+
+# * keys ------------------------------------------------------------------
+
+key1 <- read_csv("data_processed/key/key_binary-fire-code_simple_1986_2020_30m_20221031.csv",
+                 col_types = 'cccc') %>% 
+  select(-`system:index`, - .geo)
+
+
+# * rap  --------------------------------------------------------------
+
+# cover of annuals
+
+yr_res <- "_1986_2020_30m_" # time period and resolution of underlying data 
+
+afg1 <- read_csv(paste0("data_processed/RAP/RAP_AFG-by-suidBinSimple-year", 
+                        yr_res, "20221031.csv"))
+
+# process keys ------------------------------------------------------------
+
+# key matching the bin id (a long integer) to the binSimple (a short integer)
+# this key was created in 02_compile_fire_data.js
+
+key2 <- key1 %>% 
+  mutate(binSimple = as.integer(binSimple),
+         # converting numeric b/ greater precision in floats 
+         # thant 32bit integers
+         bin = as.numeric(bin),
+         years_burned = map(bin, bin_years_burned),
+         years_burned_chr = map_chr(years_burned, paste, collapse = "_"),
+         n_yrs_burned = map_dbl(years_burned, length))
+
+# only room in suidBinSimple for 5 digits, so if larger
+# binSimple value it will get cut off
+stopifnot(max(key2$binSimple) < 10^5)
+
+# years that show up as burned in the dataset
+years_burned <- key2$years_burned %>% unlist() 
+#hist(years_burned, breaks = 30)
+
+# checking that years are looking correct
+test_diff <- years_burned %>% unique() %>% sort() %>% diff
+
+stopifnot(max(years_burned) == 2020,
+          min(years_burned) == 1986,
+          # no years are missing (which would be suspicous given that
+          # over this big an area I expect something to burn every yr)
+          test_diff == 1)
+
 
 # process area ------------------------------------------------------------
 
 area2 <- area1 %>% 
-  select(area_m2, suidBin) %>% 
-  mutate(suid = str_extract(suidBin, "^\\d{6}"),
+  select(area_m2, suidBinSimple) %>% 
+  mutate(suid = str_extract(suidBinSimple, "^\\d{6}"),
          # convert back to the original suid that daniel used
          suid = as.numeric(suid) - 10^5,
          # the remaining digits are the binomial identifier code
          # that denotes which years (from 1986-2020) actually burned. 
-         bin = as.numeric(str_replace(suidBin, '^\\d{6}', "")),
-         years_burned = map(bin, bin_years_burned),
+         binSimple = as.integer(str_replace(suidBinSimple, '^\\d{6}', "")),
          area_ha = as.numeric(area_m2)/10^4) %>% 
-  
   select(-area_m2)
 
-# problem: some bin id's are zero
-area2$bin %>% summary()
-area2
 
-summary(area2$area_ha)
+# check for errors
+stopifnot(area2$binSimple %in% key2$binSimple)
 
-filter(area2, bin ==0)
+# add in information about about each binSimple
+area3 <- area2 %>% 
+  left_join(select(key2, -years_burned), by = "binSimple") %>% 
+  select(-binSimple, - bin)
 
-#--no data for 1986--this suggest an issue with the data processing!
-area2$years_burned %>% unlist() %>% unique() %>% sort()
-area2$years_burned %>% unlist() %>% hist()
+area3
 
+
+# process RAP -------------------------------------------------------------
+
+afg2 <- afg1 %>% 
+  select(-`system:index`, -.geo)
+
+# assuming all mean values are from the same image layer
+stopifnot(length(unique(afg1$bandName)) == 1)
+
+afg3 <- afg2 %>% 
+  rename(afgCov = meanValue) %>% 
+  select(-bandName)
+
+
+# save outputs ------------------------------------------------------------
+
+# info on each cluster of pixels
+write_csv(area3, paste0("data_processed/area/", area_name_base,  
+                 "info-added_", date_string, ".csv"))
+
+# RAP
+# eventually this file should include all the rap cover
+write_csv(afg3, paste0("data_processed/RAP/RAP_clean_by-suidBinSimple-year", 
+                        yr_res, date_string, ".csv"))
+afg3
