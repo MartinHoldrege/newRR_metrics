84bddc80f831b9534a9881e1c00c1d37c54c6aec
mholdrege <mholdrege@gcp.usgs.gov>
2022-12-02 18:28:17 +0000

    put meanByGroup into functions module

--- src/functions.js
+++ src/functions.js
@@ -30,3 +30,83 @@ exports.maskByFireNum = function(image, targetFireNum) {
   var out = image2.updateMask(mask);
   return out;
 };
+
+/**
+ * Means of one band by another grouping band
+ * 
+ * @param {ee.Image} image input image that contains at least two bands. must have 'year' property
+ * @param {ee.String) bandName name of band to take mean of
+ * @param {ee.String) groupName name of band that will be used for grouping (i.e. the ids)
+ * @param (ee.Feature) region to apply reducer to
+ * @param {ee.Number} scale to using when applying reducer 
+ * 
+ * @return {ee.FeatureCollection} feature collection giving mean values of bandName for 
+ * each unique value in groupName
+*/
+var meanByGroup = exports.meanByGroup = function(image, bandName, groupName, region, scale) {
+
+  // this creates a dictionary, mean value of the image for each
+  // unique set of pixels (as defined by suidBin)
+  var meanDict = ee.Image(image).select(bandName, groupName).reduceRegion({
+    reducer: ee.Reducer.mean().group({
+      groupField: 1,
+      groupName: groupName,
+    }),
+    geometry: region,
+    scale: scale,
+    maxPixels: 1e12
+  });
+  
+  // return a list where each element is a feature
+  // that contains the mean cover value, name of the image band the mean is of
+  // the suidBin, and the year the image is from
+  var meanList = ee.List(meanDict.get('groups')).map(function (x) {
+    var dict =       
+        {
+      // area in m^2
+        meanValue: ee.Dictionary(x).get('mean'),
+        bandName: bandName,
+        year: ee.Image(image).get('year')
+      };
+      // adding element to dictionary this way b/ here groupName is a variable 
+     dict[groupName] =  ee.Number(ee.Dictionary(x).get(groupName)).toInt64();
+    var f = ee.Feature(null, dict);
+    return f;
+    });
+  
+  return ee.FeatureCollection(meanList);
+};
+
+
+/**
+ * create feature collection where each feature is the mean value of a band
+ * for each year and id (from bandName)
+ * 
+ * @param {ee.ImageCollection) image collection of yearly data (e.g. RAP), must have a
+ * 'year' property
+ * @param {ee.String) bandName name of band to take mean of
+ * @param {ee.String) groupName name of band that will be used for grouping (i.e. the ids)
+ * @param (ee.Feature) region to apply reducer to
+ * @param {ee.Number} scale to using when applying reducer 
+ * 
+ * @return {ee.FeatureCollection} feature collection giving mean values of bandName for 
+ * each unique value in groupName for each year 
+*/
+exports.mapOverYears = function(ic, bandName, groupName, region, scale) {
+  
+  // mapping over years not the ic because mapping over an ic
+  // requires the output to be a feature or an image
+  // avoiding using toList() b/ of memory issues
+  var fc = years.map(function(year){
+    var image = ic// image collection (i.e. rap cover)
+      .filter(ee.Filter.eq('year',year)).first();
+    
+    // of features providing mean cover for a given suid
+    var out = meanByGroup(ee.Image(image), bandName, groupName, region, scale);
+    
+    return out;
+  });
+
+  return ee.FeatureCollection(fc).flatten();
+};
+
